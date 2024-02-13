fca78e4b1d22946309deb0c54fec8b5178cb4073
bahaali273 <bahaali273@yahoo.com>
2024-02-13 20:41:18 +0300

    search by ID from data set

--- ex3.py
+++ ex3.py
@@ -2,5 +2,8 @@ import pandas as pd
 import sqlalchemy as sa
 # name => root and localhost name of the server and maybe there's IP address from the company
 #if there is pass i have to add : and the pass
-con = sa.create_engine("mysql+pymysql://root@localhost:8080/")
+con = sa.create_engine("mysql+pymysql://root@localhost:8080/Uni")
+stdid = input("Enter Student ID")
+data = pd.read_sql("select * from students where id =" + stdid, con)
+print(data)
 
