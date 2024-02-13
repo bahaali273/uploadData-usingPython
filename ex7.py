6b79633d4a51dcd8ae355b432c259008a9d6a287
bahaali273 <bahaali273@yahoo.com>
2024-02-13 21:50:15 +0300

    read from PDF file

--- ex7.py
+++ ex7.py
@@ -1,1 +1,11 @@
 import PyPDF2
+
+with open("smart sketcher.pdf", "rb") as file:
+    pdf_reader = PyPDF2.PdfReader(file)
+    text = ""
+#pn => page number
+    for pn in range(len(pdf_reader.pages)):
+        page =pdf_reader.pages[pn]
+        text +=page.extract_text()
+
+print(text)
