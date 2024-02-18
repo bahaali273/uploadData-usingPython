164794ddfd8afbea5cacaead677cdda45554cd89
bahaali273 <bahaali273@yahoo.com>
2024-02-18 16:25:46 +0300

    using numpy, and arrays in numpy

--- ex9.py
+++ ex9.py
@@ -1,5 +1,16 @@
 # Data cleansing
 # we have to install two libraries
-#pip install numpy and matplotlib
+#pip install numpy => for mathmatical equation and matplotlib => for virtualization data
+
+import numpy as np
+ar =np.array([95, 81, 77,90,33])
+
+print(ar > 50)
+
+ar2 = np.array([2,3,1,5,3])
+print(ar +ar2)
+
+
+
 
 
