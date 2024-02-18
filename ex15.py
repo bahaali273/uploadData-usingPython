91e78f98da80e2270d8462af956e77444f8c06cb
bahaali273 <bahaali273@yahoo.com>
2024-02-18 20:28:02 +0300

    remove columns

--- ex15.py
+++ ex15.py
@@ -3,3 +3,10 @@ import pandas as pd
 
 data = pd.read_csv("train.csv")
 print(data.shape)
+print(data.isnull().sum())
+#remove columns
+#data2= data.drop(columns=["Cabin"])
+data.drop(columns=["Cabin"],inplace=True)
+#We can use inplace if we dont need the colunm at all
+print("After dropping")
+print(data.isnull().sum())
