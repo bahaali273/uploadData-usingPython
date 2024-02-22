17eb478f7dc075dd167eff26bd42e06e1a22532d
bahaali273 <bahaali273@yahoo.com>
2024-02-22 20:59:36 +0300

    Correlation

--- ex22.py
+++ ex22.py
@@ -4,7 +4,16 @@ import pandas as pd
 
 data = pd.read_csv("house.csv")
 # General information about your file #   Column  Non-Null Count  Dtype
+print("Data Info")
 print(data.info())
 #data statistics
 print("data statistics")
 print(data.describe())
+
+#Correlation => relation between different columns and it will be between (-1 to 1)
+
+cor_matrix = data.corr()
+print("Correlation Matrix : ")
+print(cor_matrix)
+
+
