1fc4e9c57f9fcf18a06389942657ca2512d4de28
bahaali273 <bahaali273@yahoo.com>
2024-02-25 21:14:59 +0300

    more comments

--- ex25.py
+++ ex25.py
@@ -1,7 +1,18 @@
 #Data Analytics # data is not exist
 # using ML OR statical (Stats methods)
 #predect result, like predect net profiet for the bank statment for next year
-# How much / how many OR is it A or B, OR which group it belong to ?
-# OR IS it weired ? OR what is next? this question Data Analytics can awnser this.
-
-
+# How much / how many?, OR is it A or B, OR which group it belong to ?
+# OR IS it weired ? OR what is next (Action Plan?(DSS) this question Data Analytics can awnser this.
+#pip install statsmodels
+# Data= input + output => ex. sqft,bdrms,sge => price
+# after that we have to do cleansing and OLS returns Weights (W) + Biase (b)
+#number of Weights = number of inputs
+# ex for one inputs =>y^ =b + x1 * w1 / 2 inputs or more =>x1 * w1 +x2 * w2 + x3 * w3 ,
+# y^ = predected
+import random
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+random.seed(10)
+print(random.random())
+plt.show()
