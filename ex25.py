55fdf90e1488faf7eaf43aa0ce89ba09575dd549
bahaali273 <bahaali273@yahoo.com>
2024-02-25 20:23:51 +0300

    Data Analytics explanation

--- ex25.py
+++ ex25.py
@@ -1,4 +1,7 @@
 #Data Analytics # data is not exist
 # using ML OR statical (Stats methods)
 #predect result, like predect net profiet for the bank statment for next year
-#
+# How much / how many OR is it A or B, OR which group it belong to ?
+# OR IS it weired ? OR what is next? this question Data Analytics can awnser this.
+
+
