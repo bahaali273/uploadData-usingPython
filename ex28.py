c85910b6a9bc74265eecc8688674bec0c408f00d
bahaali273 <bahaali273@yahoo.com>
2024-02-29 16:43:58 +0300

    accuracy

--- ex28.py
+++ ex28.py
@@ -14,11 +14,14 @@ import statsmodels.api as sm
 s = sm.add_constant(x)
 res = sm.Logit(y, s).fit()
 print(res.summary())
-
+# (100 - accuracy "Pseudo R-squ") / 2 => ex. 100 -82 =18 /2 = 9 => after this we have to add this number to 50% and minimize it from 50%
 import numpy as np
 np.set_printoptions(formatter={'float':lambda x:"{0:0.3f}".format(x)})
 cls_result = res.predict()
 print(cls_result)
+
+# to change this accuracy number
+print(cls_result > 0.59)
 
 plt.scatter(data["SAT"], data["Admitted"])
 plt.show()
