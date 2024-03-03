efba6eb299e502351b87e44b63a43f5743085d77
bahaali273 <bahaali273@yahoo.com>
2024-03-03 20:47:11 +0300

    Epochs and Learning Rate

--- ex33.py
+++ ex33.py
@@ -1,5 +1,8 @@
 #AI => Machine Learning (Data + Model) =>Hyper Parameters
 #Experiments (Trying)
 
-#(Epochs and Learning Rate)
-#      Epochs
+#(Epochs(Weights) and Learning Rate(0-1)
+#      Epochs  => ex (1 epoch) that mean how many AI can read data; trying one time: how many time AI can Learn.
+#      Learning Rate => how fast AI can learn model can go through data fast or not
+
+
