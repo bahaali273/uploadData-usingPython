5f369ee287ba419478bcc6308f601e829cec50f9
bahaali273 <bahaali273@yahoo.com>
2024-03-03 21:59:02 +0300

    bild model from zero

--- ex34.py
+++ ex34.py
@@ -3,10 +3,10 @@
 import pandas as pd
 data = pd.read_csv("wh.csv")
 x= data["Height"]
-y= data["weight"]
+y= data["Weight"]
 
 from keras.models import Sequential
-# Sequential from input to output one to another
+# Sequential from input to output one to another like neural network
 from keras.layers import Dense
 #Dense to give output from Input
 from keras.optimizers import Adam
@@ -14,5 +14,7 @@ from keras.optimizers import Adam
 # We will start with Sequential single lain one input and output
 
 model = Sequential()
-model.add(Dense(1, input_shape=(128,128,20))) # 1 output and 1 input
-model.compile(Adam(Learning_rate=0.5),loss="mean_squared_error")
+model.add(Dense(1, input_shape=(1,))) # 1 output and 1 input
+model.compile(Adam(learning_rate=0.5), loss="mean_squared_error")
+
+model.fit(x, y,epochs=100)
