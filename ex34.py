82779a302c94c786bc1ec2b014d91f0b5d5ba2ac
bahaali273 <bahaali273@yahoo.com>
2024-03-05 20:26:14 +0300

    Single prediction:

--- ex34.py
+++ ex34.py
@@ -16,5 +16,21 @@ from keras.optimizers import Adam
 model = Sequential()
 model.add(Dense(1, input_shape=(1,))) # 1 output and 1 input
 model.compile(Adam(learning_rate=0.5), loss="mean_squared_error")
-
+# epochs => how many times i need to train the model
 model.fit(x, y,epochs=100)
+
+#comper result after and befor
+yp = model.predict(x)
+#to find the accuricy
+import sklearn.metrics as mtr
+print(mtr.r2_score(y, yp))
+
+import matplotlib.pyplot as plt
+
+plt.scatter(x,y)
+plt.plot(x,yp, c="red")
+plt.show()
+
+
+#Single prediction
+
