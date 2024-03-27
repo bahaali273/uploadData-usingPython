e224ecda53af2b50072c4deaa5f56139b54c94fc
bahaali273 <bahaali273@yahoo.com>
2024-03-27 14:55:24 -0700

    convert text to numbers

--- ex36.py
+++ ex36.py
@@ -4,4 +4,15 @@ import pandas as pd
 
 data = pd.read_csv("iris.csv")
 # new strategy: if I have too many columns like 20 and the output in the end 21 i have to use this way
-x = data
+# mean give me all the columns except [species] and i can add more if i want
+x = data.drop(columns=["species"])
+y= data["species"]
+
+
+#Now i have to covert text to numbers
+
+target_name = data["species"].unique()
+#unique() => data without repation
+print(target_name)
+
+y= data["species"].map({'setosa':1, 'versicolor':2, 'virginica':3})
