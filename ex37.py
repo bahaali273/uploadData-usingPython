eb12e19f4580367f86675b01592ea1542106a94d
bahaali273 <bahaali273@yahoo.com>
2024-04-03 00:48:22 -0700

    Neural Network

---
+++ ex37.py
@@ -0,0 +1,15 @@
+# Neural network => input layer +  HIDDEN LAYERS(like black box) + output layer
+
+#Hyper Parameters 1- Learining Rate 2- Epochs 3-Number of hidden layers =>works layer by layer if i need i can add new layer
+#acctivation caled (RELO)
+
+
+#example
+#input =8
+#hidden Layer 1 =9
+#hidden Layer 2 =9
+#hidden Layer 3 =9
+#output layer = 4
+# 8*9 + 9*9 + 9*9 + 9*4 =270 time in each epoch
+
+#Propagrarion  (forward + Backward)
